f1df170fa7a49fbdfd39625be8605e8f3c9ea606
Krisztian Benda <krisztianbenda@gmail.com>
2018-11-25 18:55:40 +0100

    ADD: final docs and src

--- src/product/app/label.py
+++ src/product/app/label.py
@@ -5,7 +5,7 @@ import wikipedia as wiki
 from unidecode import unidecode
 import googlemaps
 
-google_api_key = 'AIzaSyDHPFTie9AvvVFqXTCI5a43UBI8qkzLvXk'
+google_api_key = ''
 gmaps = googlemaps.Client(key=google_api_key)
 
 
@@ -25,17 +25,17 @@ class Label:
             return self.build_search_link()
 
     def build_maps_link(self):
-        # geocode_results = gmaps.geocode(self.name)
-        # Erdetileg ez lenne a valasz a Mount Everestre:
-        geocode_results = [{'address_components': [
-            {'long_name': 'Mount Everest', 'short_name': 'Monte Everest',
-             'types': ['establishment', 'natural_feature']}],
-            'formatted_address': 'Mt Everest',
-            'geometry': {'location': {'lat': 27.9881206, 'lng': 86.9249751}, 'location_type': 'APPROXIMATE',
-                         'viewport': {'northeast': {'lat': 27.9979732, 'lng': 86.94098249999999},
-                                      'southwest': {'lat': 27.9782671, 'lng': 86.90896769999999}}},
-            'place_id': 'ChIJvZ69FaJU6DkRsrqrBvjcdgU', 'plus_code': {'global_code': '7MV8XWQF+6X'},
-            'types': ['establishment', 'natural_feature']}]
+        geocode_results = gmaps.geocode(self.expression)
+        # Here is an example for Mount Everest:
+        # geocode_results = [{'address_components': [
+        #     {'long_name': 'Mount Everest', 'short_name': 'Monte Everest',
+        #      'types': ['establishment', 'natural_feature']}],
+        #     'formatted_address': 'Mt Everest',
+        #     'geometry': {'location': {'lat': 27.9881206, 'lng': 86.9249751}, 'location_type': 'APPROXIMATE',
+        #                  'viewport': {'northeast': {'lat': 27.9979732, 'lng': 86.94098249999999},
+        #                               'southwest': {'lat': 27.9782671, 'lng': 86.90896769999999}}},
+        #     'place_id': 'ChIJvZ69FaJU6DkRsrqrBvjcdgU', 'plus_code': {'global_code': '7MV8XWQF+6X'},
+        #     'types': ['establishment', 'natural_feature']}]
         if len(geocode_results) == 0:
             '''Location Not Found => we just search for it on Google'''
             return self.build_wiki_link()
